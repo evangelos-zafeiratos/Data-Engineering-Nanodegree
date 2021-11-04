543a62bf9bee4f4105cc5e4de1679995868a620b
Evangelos Zafeiratos <74437675+evangelos-zafeiratos@users.noreply.github.com>
2021-11-04 21:13:44 +0100

    Add files via upload

--- Data Modeling with Postgres/scripts/etl.py
+++ Data Modeling with Postgres/scripts/etl.py
@@ -1,47 +1,63 @@
 import os
 import glob
 import psycopg2
+from datetime import datetime
 import pandas as pd
 from sql_queries import *
 
+song_index = 1
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines = True)
 
     # insert song record
-    song_data = 
+    song_data = df[["song_id", "title", "artist_id", "year", "duration"]].values[0]
+    song_data = song_data.tolist()
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data = df[["artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude" ]].values[0]
+    artist_data = artist_data.tolist()
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines = True)
 
     # filter by NextSong action
-    df = 
+    df = df[df['page'] == 'NextSong']
 
     # convert timestamp column to datetime
-    t = 
+    time_data = list()
+    timestamp = df["ts"].values.tolist()
+    for value in timestamp:
+        t = list()
+        dt_object = datetime.fromtimestamp(value/1000.0)
+        time_obj = dt_object.time()
+        t.append(value)
+        t.append(dt_object.hour)
+        t.append(dt_object.day)
+        t.append(dt_object.isocalendar()[1])
+        t.append(dt_object.month)
+        t.append(dt_object.year)
+        t.append(dt_object.isoweekday())
+        time_data.append(t)
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    column_labels = ["start_time", "hour", "day", "week", "month", "year", "weekday"]
+    time_df = pd.DataFrame(time_data, columns = column_labels)
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[["userId", "firstName", "lastName", "gender", "level" ]]
 
     # insert user records
     for i, row in user_df.iterrows():
-        cur.execute(user_table_insert, row)
+        cur.execute(user_table_insert, list(row))
 
     # insert songplay records
     for index, row in df.iterrows():
@@ -56,8 +72,10 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        global song_index
+        songplay_data = [song_index, row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent]
         cur.execute(songplay_table_insert, songplay_data)
+        song_index += 1
 
 
 def process_data(cur, conn, filepath, func):
@@ -82,6 +100,8 @@ def process_data(cur, conn, filepath, func):
 def main():
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
+    
+    
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
     process_data(cur, conn, filepath='data/log_data', func=process_log_file)
