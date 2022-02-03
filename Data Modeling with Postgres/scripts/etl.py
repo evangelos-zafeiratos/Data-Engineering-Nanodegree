90aca199c45c94a6e1dea6985bc6a8b03193abbf
Evangelos Zafeiratos <74437675+evangelos-zafeiratos@users.noreply.github.com>
2022-02-03 03:08:04 +0200

    Changes after Review
    - Add docstrings for each function
    
    - Remove songplay_id variable

--- Data Modeling with Postgres/scripts/etl.py
+++ Data Modeling with Postgres/scripts/etl.py
@@ -8,6 +8,18 @@ from sql_queries import *
 song_index = 1
 
 def process_song_file(cur, filepath):
+    """
+    - Opens song file
+
+    - Parses file and extracts list of values for song table
+
+    - Calls song_table_insert function to insert values
+
+    - Parses file and extracts list of values for artist table
+
+    - Calls song_table_insert to insert values
+
+    """
     # open song file
     df = pd.read_json(filepath, lines = True)
 
@@ -15,7 +27,7 @@ def process_song_file(cur, filepath):
     song_data = df[["song_id", "title", "artist_id", "year", "duration"]].values[0]
     song_data = song_data.tolist()
     cur.execute(song_table_insert, song_data)
-    
+
     # insert artist record
     artist_data = df[["artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude" ]].values[0]
     artist_data = artist_data.tolist()
@@ -23,6 +35,20 @@ def process_song_file(cur, filepath):
 
 
 def process_log_file(cur, filepath):
+    """
+    - Opens log file and selects only 'NextSong' records
+
+    - Using datetime library breaks down the timestamp variable into multiple requested values and save into a dataframe
+
+    - Iterate through dataframe to insert values into time table.
+
+    - Create a new dataframe using only values requested for user table.
+
+    - Iterate through dataframe to insert values into user table.
+
+    - Finally, iterate through dataframe and populate songplay table via calling songplay_table_insert.
+
+    """
     # open log file
     df = pd.read_json(filepath, lines = True)
 
@@ -44,7 +70,7 @@ def process_log_file(cur, filepath):
         t.append(dt_object.year)
         t.append(dt_object.isoweekday())
         time_data.append(t)
-    
+
     # insert time data records
     column_labels = ["start_time", "hour", "day", "week", "month", "year", "weekday"]
     time_df = pd.DataFrame(time_data, columns = column_labels)
@@ -61,24 +87,28 @@ def process_log_file(cur, filepath):
 
     # insert songplay records
     for index, row in df.iterrows():
-        
+
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
-        
+
         if results:
             songid, artistid = results
         else:
             songid, artistid = None, None
 
         # insert songplay record
-        global song_index
-        songplay_data = [song_index, row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent]
+        songplay_data = [row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent]
         cur.execute(songplay_table_insert, songplay_data)
-        song_index += 1
 
 
 def process_data(cur, conn, filepath, func):
+    """
+    - Add all json files in a list
+
+    - Iterate through files and process.
+
+    """
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -98,10 +128,18 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
+    """
+    - Make a connection to the database.
+
+    - Call function for processing song data.
+
+    - Call function for processing log data.
+
+    """
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
-    
-    
+
+
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
     process_data(cur, conn, filepath='data/log_data', func=process_log_file)
